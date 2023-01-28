b7ed5a4e0aa44c591081d175c5cb9b7c788986c8
mahboobe-Mhz <mahbo0be.mohammadzade@gmail.com>
2023-01-28 17:12:04 +0330

    answring Q4

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 "use strict";
+//-------------------------Question-4---------------------
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+
+function aclean(arr) {
+  let map = new Map();
+  let word;
+  arr.forEach((element) => {
+    word = element.toLowerCase().split("").sort().join("");
+
+    map.set(word, element);
+  });
+
+  let finalArr = Array.from(map.values());
+  console.log(finalArr);
+}
+aclean(arr);
