04a812f2b20a01b91dec93d8962aa25433c39c15
mahboobe-Mhz <mahbo0be.mohammadzade@gmail.com>
2023-01-28 16:20:12 +0330

    answering Q3

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 "use strict";
+//-------------------------Question-3----------------------
+let arr = [
+  "Hare",
+  "Krishna",
+  "Hare",
+  "Krishna",
+  "Krishna",
+  "Krishna",
+  "Hare",
+  "Hare",
+  ":-O",
+];
+function unique(arr) {
+  const setArr = new Set(arr);
+
+  console.log(setArr);
+}
+unique(arr);
