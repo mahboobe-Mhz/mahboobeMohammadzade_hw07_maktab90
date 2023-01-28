a68d634ca5f4d42b661edde48cff7f068265b490
mahboobe-Mhz <mahbo0be.mohammadzade@gmail.com>
2023-01-28 10:54:37 +0330

    answering Q1

---
+++ app.js
@@ -0,0 +1,20 @@
+"use strict";
+//--------------------------Question-1--------------------
+let salaries = {
+  John: 100,
+  Pete: 300,
+  Mary: 250,
+};
+function topSalary(salaries) {
+  let maxSalary = 0;
+  let maxName = null;
+  for (const [name, salary] of Object.entries(salaries)) {
+    if (maxSalary < salary) {
+      maxSalary = salary;
+      maxName = name;
+    }
+  }
+
+  console.log(maxName);
+}
+topSalary(salaries);
