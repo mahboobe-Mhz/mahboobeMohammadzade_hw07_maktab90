7c7f356c33c5a3d582ff15be01e2ffc68805838e
mahboobe-Mhz <mahbo0be.mohammadzade@gmail.com>
2023-01-29 14:38:14 +0330

    answering Q5

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 "use strict";
+//----------------------Question-5-------------
+let map = new Map();
+map.set("name", "John").set("name2", "jack");
+
+let arrKeys = Array.from(map.keys());
+console.log(arrKeys);
