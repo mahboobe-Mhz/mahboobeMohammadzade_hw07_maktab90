e43b0046f7112d4cf3dfe564800288c1d0473176
mahboobe-Mhz <mahbo0be.mohammadzade@gmail.com>
2023-01-29 22:29:38 +0330

    Q2 addition

--- app.js
+++ app.js
@@ -3,10 +3,10 @@
 const arr1 = [5, 6, 2];
 const arr2 = [3, 7, 1];
 const arr3 = [2, 4, 8];
-function sorting(arr1, arr2, arr3) {
+function sorting(...arr) {
   let sumArr = [];
   //merge input array with concat
-  sumArr = sumArr.concat(arr1, arr2, arr3);
+  sumArr = sumArr.concat(...arr);
   //sorting our array
   sumArr.sort(compareNumeric);
   console.log(sumArr);
